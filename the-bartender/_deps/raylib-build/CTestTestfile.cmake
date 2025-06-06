76c23d65eca4adcdf1019ca69b620102861c67c7
EmersonMR25 <eam341@georgetown.edu>
2025-06-06 05:46:55 -0400

    feat: added tasks.json for faster build operations

---
+++ the-bartender/_deps/raylib-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/abdiel/Desktop/Games/the-bartender/the-bartender/_deps/raylib-src
+# Build directory: /Users/abdiel/Desktop/Games/the-bartender/the-bartender/_deps/raylib-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
